0a5e733b6c4667080f697f35bc7b0313afb24625
Eray İbrahimoğlu <ibrahimoglu.eray@gmail.com>
2021-03-11 01:58:17 +0000

    bugfix

--- testblescan.py
+++ testblescan.py
@@ -12,13 +12,9 @@ import json
 import socket
 import sys
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ( "192.168.10.101" , 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
+
+
 
 dev_id = 0
 
@@ -39,29 +35,38 @@ blescan.hci_enable_le_scan(sock_bluetooth)
 
 
 
+while True:
+    returnedList = blescan.parse_events(sock_bluetooth, 200)
 
-returnedList = blescan.parse_events(sock_bluetooth, 100)
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print (returnedList)
+    # Connect the socket to the port where the server is listening
+    server_address = ( "192.168.10.101" , 10000)
+    print >>sys.stderr, 'connecting to %s port %s' % server_address
+    sock.connect(server_address)
 
-try:
+    try: 
 
-    # Send data
-    print >>sys.stderr, 'sending message'
-    sock.sendall(json.dumps(returnedList))
+        # Send data
+        print >>sys.stderr, 'sending message'
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(returnedList)
-    
-    # while amount_received < amount_expected:
-    #    data = sock.recv(16)
-    #    amount_received += len(data)
-    #    print >>sys.stderr, 'received "%s"' % data
+        if len(returnedList):
+            sock.sendall(json.dumps(returnedList))
+            print(returnedList)
 
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(returnedList)
+        
+        # while amount_received < amount_expected:
+        #    data = sock.recv(16)
+        #    amount_received += len(data)
+        #    print >>sys.stderr, 'received "%s"' % data
+
+    finally:
+        print >>sys.stderr, 'closing socket'
+        sock.close()
 
     #clear()
 
