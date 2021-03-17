4c4fcb57a86af59f1c1a4f9b74fce7cb19f6716c
furkan koçak <furkkocak@gmail.com>
2021-03-17 18:40:15 +0300

    add droplist selection, Enable and Disable Buttons

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import *
+#from tkinter import *
 import os
 import json
  
@@ -23,26 +23,27 @@ image = canvas.create_image(x + 53, y + 621, image=img)
 
 
 
-# pop a window up when the button "New Visitor" is pushed
-def getfromUser():
+#pop a window up when the button "Enable a Device" is pushed
+def EnableDevice():
+    
     window =tk.Tk()
-    # canvas = tk.Canvas(window, width=100, height=200, bg="grey")
-    # canvas.pack() # this makes it visible
     window.title("Enter new visitor info")
     window.geometry("600x300")
+    #window.protocol ('WM_DELETE_WINDOW', (lambda: 'pass') ())
     
+          
     L1=tk.Label(window,text="Username:",font=16)
     L2=tk.Label(window,text="Password:",font=16)
     L1.grid(row=0,column=0,padx=5,pady=5)
     L2.grid(row=1,column=0,padx=5,pady=5)
     
-    
+        
     username=tk.StringVar(window)  
     password=tk.StringVar(window)
     
     # get entry from user
-    t1= tk.Entry(window,textvariable=username,font=16)
-    t2= tk.Entry(window,textvariable=password,font=16,show="*")
+    t1= tk.Entry(window,textvariable=username,font=16,width=30)
+    t2= tk.Entry(window,textvariable=password,font=16,show="*",width=30)
     t1.grid(row=0,column=1)
     t2.grid(row=1,column=1)
     
@@ -61,12 +62,73 @@ def getfromUser():
         else:
             tk.messagebox.showwarning(title="Cancelled",message="Please enter new visitor data")
             
+    def selection():
+        tk.messagebox.showinfo(title="Device Manager",message=clicked.get() + " has been enabled")
+        label=tk.Label(window,text=clicked.get() + " has been activated",font=16)
+        label.grid(row=5,column=1,padx=5,pady=5)
+    
     #make buttons for the user to click when done
     buttonEnter=tk.Button(window,command=Check,text="Enter",font="16")
     buttonCancel=tk.Button(window,command=Cancel,text="Cancel",font="16")
     buttonEnter.grid(row=2,column=1,sticky="w")     #w = west
     buttonCancel.grid(row=2,column=1,sticky="e")    #e = east
+    
+    
+    #Drop Down Box
+    options= ["Device 1", 
+              "Device 2",
+              "Device 3",
+              "Device 4",
+              "Device 5", 
+              "Device 6",
+              "Device 7",
+              "Device 8"
+        ]
+    clicked=tk.StringVar(window)
+    clicked.set(options[0])
+    option=tk.OptionMenu(window,clicked ,*options )
+    option.grid(row=3,column=1)
+    
+    
+    buttonSelectDrop=tk.Button(window,text="Select",command=selection)
+    buttonSelectDrop.grid(row=4,column=3,sticky="w")
 
+    #  # #to prevent opening multiple child windows 
+    # window.transient()
+    # window.focus()
+    # window.grab_set()
+    # root.wait_window(window)
+   
+    window.mainloop()
+
+def DisableDevice():
+    window =tk.Tk()
+    window.title("Enter new visitor info")
+    window.geometry("400x150")
+    
+    #Drop Down Box
+    options= ["Device 1", 
+              "Device 2",
+              "Device 3",
+              "Device 4",
+              "Device 5", 
+              "Device 6",
+              "Device 7",
+              "Device 8"
+        ]
+    clicked=tk.StringVar(window)
+    clicked.set(options[0])
+    option=tk.OptionMenu(window,clicked ,*options )
+    option.grid(row=0,column=0,padx=10,pady=10)
+    
+    
+    def selection():
+        label=tk.Label(window,text=clicked.get() + " has been disabled",font=16)
+        tk.messagebox.showinfo(title="Device Manager",message=clicked.get() + " has been disabled")
+        label.grid(row=1,column=1,padx=5,pady=5)
+    
+    buttonSelectDrop=tk.Button(window,text="Select",command=selection)
+    buttonSelectDrop.grid(row=0,column=1,sticky="w")
 
    
     window.mainloop()
@@ -81,17 +143,25 @@ def move(event = "none"):
     y = data["y"]
     root.after(1000, move)
     
-# get entry from user
-enter= tk.Entry(root,width=30, bg="green",fg="white",borderwidth= 10)
-enter.pack()
+# # get entry from user
+# enter= tk.Entry(root,width=30, bg="green",fg="white",borderwidth= 10)
+# enter.pack()
 
-newVisitor= tk.Button(root,text="New Visitor", padx=10, pady=5, fg="white",
-                      bg="#204D40" ,command=getfromUser)
+newVisitor= tk.Button(root,text="Enable a Device", padx=10, pady=5, fg="white",
+                      bg="#204D40",command=EnableDevice)
 
 newVisitor.pack()
+
+
+disableVisitor= tk.Button(root,text="Disable a Device", padx=10, pady=5, fg="white",
+                      bg="#204D40" ,command=DisableDevice )
+
+disableVisitor.pack()
+
  
 # This bind window to keys so that move is called when you press a key
-root.after(10, move)
+root.after(1000, move)
+
  
 # this creates the loop that makes the window stay 'active'
 root.mainloop()
