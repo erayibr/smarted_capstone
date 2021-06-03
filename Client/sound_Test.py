189282f8f5ee455d6a6d662f0378a3602fed8919
erayibr <ibrahimoglu.eray@gmail.com>
2021-06-03 01:07:12 +0100

    subprocess fix

--- Client/sound_Test.py
+++ Client/sound_Test.py
@@ -1,6 +1,5 @@
 import subprocess
 import time
-import os
 import subprocess
 
 file_name_temp = "asdasd"
@@ -11,16 +10,17 @@ while True:
     file_name = str(f.read()) + ".wav"
     f.close()
     time.sleep(1)
-    print(file_name)
+    # print(file_name)
 
-    if ((file_name != file_name_temp) and (file_name != "none.wav")):
-        if dirty:
+    if (file_name != file_name_temp):
+        if dirty and process.poll() == None:
             process.stdin.write('q')
             print("killed: " + file_name_temp)
         dirty = 1
-        process = subprocess.Popen(["omxplayer", "-o",  "local",  file_name], stdin=subprocess.PIPE)
-        print(file_name)
-        print(file_name_temp)
+        if file_name != "none.wav":
+            process = subprocess.Popen(["omxplayer", "-o",  "local",  file_name], stdin=subprocess.PIPE)
+        # print(file_name)
+        # print(file_name_temp)
         file_name_temp = file_name
     else:
         continue
